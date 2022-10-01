8b381abee7ef2146816548b3c0d7c8716d56435a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-10-01 11:39:36 -0700

    Data Organizing
    Going to organize the object information a bit better, this is the starting poing for that.
    These testing changes temporarily only show the resulting first chunk in the region you are trying to read.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
 export * from "./read.js";
 export { chunkify } from "gamedata-parser";
 
-export class Chunk {
-  /**
-   * @param { Uint8Array } [header]
-   * @param { Uint8Array } [data]
-  */
-  constructor(public header: Uint8Array, public data: Uint8Array) {
-    this.header = header;
-    this.data = data;
-  }
+export class Region {
+  constructor(public locations: Uint8Array[] = [], public chunks: Chunk[] = []) {}
 }
 
-export class Region {
-  /**
-   * @param { Uint8Array[] } [locations]
-   * @param { Chunk[] } [chunks]
-  */
-  constructor(public locations: Uint8Array[] = [], public chunks: Chunk[] = []) {
-    this.locations = locations;
-    this.chunks = chunks;
-  }
+export class Chunk {
+  constructor(public header: Uint8Array, public data: Uint8Array) {}
 }
--- src/read.ts
+++ src/read.ts
@@ -2,26 +2,26 @@ import * as zlib from "node:zlib";
 import { chunkify, Chunk, Region } from "./index.js";
 
 export async function read(data: Uint8Array){
-  const locations = getLocations(new Uint8Array(data.subarray(0,4096)));
+  const locations = [getLocations(new Uint8Array(data.slice(0,4096))).map(Buffer.from)[0]];
   // console.log(locations);
   const result = new Region(locations);
 
-  for (const [i,location] of locations.entries()){
+  for (const location of locations){
     const offset = new DataView(new Uint8Array([0,...location.slice(0,3)]).buffer).getUint32(0) * 4096;
     const size = location[3] * 4096;
     if (offset === size) continue;
     // console.log(offset,size);
 
-    const chunk = Buffer.from(data.subarray(offset,offset + size));
+    const chunk = data.slice(offset,offset + size);
 
-    const header = data.subarray(0,12);
+    const header = data.slice(0,12);
     // console.log(header);
 
-    const content = zlib.inflateRawSync(chunk.subarray(12));
+    const content = zlib.inflateRawSync(chunk.slice(12));
     // console.log(content,"\n");
 
     result.chunks.push(new Chunk(header,content));
-    // if (i === 1) break;
+    break;
   }
 
   return result;
