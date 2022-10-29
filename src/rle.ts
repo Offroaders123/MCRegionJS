5abc1c21befcb243275b0acea1e123b6ed50618a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-10-28 18:28:11 -0700

    RLEify
    Redid RLE a little bit.

--- src/rle.ts
+++ src/rle.ts
@@ -1,4 +1,5 @@
-export function decode(data: Uint8Array, { compressed, decompressed }: { compressed: number; decompressed: number; }){
+export function decode(data: Uint8Array, decompressed: number){
+  const compressed = data.byteLength;
   const result = new Uint8Array(decompressed);
   let i = 0;
   let offset = 0;
@@ -7,15 +8,14 @@ export function decode(data: Uint8Array, { compressed, decompressed }: { compres
     const entry = data[i];
     const length = data[i + 1];
 
-    if (entry === 0xff && length >= 3){
+    if (entry === 0xFF && length >= 3){
       const value = data[i + 2];
-      console.log(`Wrap! Length: ${length}; Value: ${value};`);
-      result.set(Array(length).fill(value),offset);
+      const entries = Array<number>(length).fill(value);
 
+      result.set(entries,offset);
       i += 2;
-      offset += length - 1;
+      offset += (length - 1);
     } else {
-      console.log(i,offset,entry.toString(16).padStart(2,"0"));
       result[offset] = entry;
     }
 
--- test/chunk.js
+++ test/chunk.js
@@ -1,29 +1,29 @@
 // @ts-check
 
-import * as fs from "node:fs/promises";
-import * as zlib from "node:zlib";
-import * as rle from "../dist/rle.js";
+import { readFile, writeFile } from "node:fs/promises";
+import { inflateRawSync } from "node:zlib";
+import { decode } from "../dist/rle.js";
 
 const decoder = new TextDecoder();
 
-const data = await fs.readFile(new URL("./chunk/chunk.bin",import.meta.url))
+const data = await readFile(new URL("./chunk/chunk.bin",import.meta.url))
 .then(data => {
-  const length = new DataView(data.buffer).getUint32(0) & 0xffffff;
+  const length = new DataView(data.buffer).getUint32(0) & 0xFFFFFF;
   return data.subarray(0,length + 8);
 })
 .then(data => {
   const decompressed = new DataView(data.buffer).getUint32(4);
-  const result = zlib.inflateRawSync(data.subarray(12));
+  const result = inflateRawSync(data.subarray(12));
   const compressed = result.byteLength;
   return { compressed, decompressed, data: result };
 })
 .then(({ compressed, decompressed, data }) => {
-  const result = Buffer.from(rle.decode(data,{ compressed, decompressed }));
+  const result = Buffer.from(decode(data,decompressed));
   console.log(compressed,decompressed,"\n");
   return result;
 });
 console.log(data.byteLength,"\n");
 console.log(data,"\n");
 
-// console.log(decoder.decode(data),"\n");
-// await fs.writeFile("./rle-decompress-v5.bin",data);
+console.log(decoder.decode(data),"\n");
+// await writeFile("./rle-decompress-v6.bin",data);
