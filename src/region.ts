e9a4329466b6c2545ee04343ab9083f529a1fc3d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-12-22 13:03:24 -0800

    Implementation Abstractions
    Started making some of the implementation details for how the data is read more hidden, since it's not too user-friendly to be accessible. At least, not quite yet :)
    
    Did some looking into Promise chaining and how async functions work. This article is super cool!
    https://www.bennadel.com/blog/3835-returning-promises-from-async-await-functions-in-javascript.htm

--- src/region.ts
+++ src/region.ts
@@ -8,24 +8,24 @@ export interface Location {
 export class Region extends Array<Chunk> {
   static async read(data: Uint8Array) {
     const locations = this.readLocations(data);
-    const chunks = await Promise.all(locations.map(location => this.readChunk(data,location)));
+    const chunks = await Promise.all(locations.map(location => this.#readChunk(data,location)));
     return new Region(...chunks);
   }
 
-  static getLocations(data: Uint8Array) {
+  static #getLocations(data: Uint8Array) {
     return data.subarray(0,4096);
   }
 
   static readLocations(data: Uint8Array) {
-    const locations = this.getLocations(data);
+    const locations = this.#getLocations(data);
     const view = new DataView(locations.buffer,locations.byteOffset,locations.byteLength);
     const offset = view.getUint8(0);
 
     const result: Location[] = [];
 
     for (let i = offset; i < locations.byteLength; i += 4){
-      const data = this.getLocation(locations,i);
-      const location = this.readLocation(data);
+      const data = this.#getLocation(locations,i);
+      const location = this.#readLocation(data);
       result.push(location);
       break; // For testing
     }
@@ -33,23 +33,23 @@ export class Region extends Array<Chunk> {
     return result;
   }
 
-  static getLocation(data: Uint8Array, offset: number) {
+  static #getLocation(data: Uint8Array, offset: number) {
     return data.subarray(offset,offset + 4);
   }
 
-  static readLocation(data: Uint8Array): Location {
+  static #readLocation(data: Uint8Array): Location {
     const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
     const offset = view.getUint32(0) & 0xFFFFFF * 4096;
     const length = view.getUint8(3) * 4096;
     return { offset, length };
   }
 
-  static getChunk(data: Uint8Array, { offset, length }: Location) {
+  static #getChunk(data: Uint8Array, { offset, length }: Location) {
     return data.subarray(offset,offset + length);
   }
 
-  static async readChunk(data: Uint8Array, { offset, length }: Location) {
-    const chunk = this.getChunk(data,{ offset, length });
-    return await Chunk.read(chunk);
+  static async #readChunk(data: Uint8Array, { offset, length }: Location) {
+    const chunk = this.#getChunk(data,{ offset, length });
+    return Chunk.read(chunk);
   }
 }
