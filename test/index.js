b3548f53f2a3fd2d3c224e66767be19742e46745
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-12-29 01:09:20 -0800

    Chunk Location Offset Debug Session
    Got some more help from UtterEvergreen, my chunk location byte offset calculation code was super wonk. Gonna commit these while they appear to be correct. Not gonna mess with it without saving it first :)

--- test/index.js
+++ test/index.js
@@ -7,8 +7,25 @@ const data = await fs.readFile(new URL("./world/r.0.0.mcr",import.meta.url))
   .then(buffer => new Uint8Array(buffer));
 // console.log(data);
 
-const region = await Region.read(data);
+// const region = Region.readLocations(data);
 
-for (const { header } of region){
-  console.log(header);
+// for (const location of region){
+//   console.log(location);
+// }
+
+const content = data.subarray(0,4096);
+const view = new DataView(content.buffer,content.byteOffset,content.byteLength);
+
+let result = "";
+
+for (let i = 0; i < content.byteLength; i += 4){
+  // result += `${data.subarray(i,i + 4).join(" ")}\n`;
+  // result += `${new Int32Array(data.buffer.slice(i,i + 4))}\n`;
+  const location = view.getUint32(i);
+  // Thanks UtterEvergreen!!!
+  const offset = (location >> 8) * 4096;
+  const length = (location & 255) * 4096;
+  result += `${offset} ${length}\n`;
 }
+
+console.log(result);
