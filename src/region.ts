fbdcccaec04a12e39435ebe18c927073a9082b88
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-12-29 01:45:23 -0800

    Chunk Location Offset Implementation
    Now, on to the app implementation of the bug fix! It actually appears to be working so far!
    
    I was calculating the chunk locations offset section wrong, and this was throwing off the rest of the reading process. So, turns out it was the `Location.offset` calculation that was incorrect, rather than the chunk reading part.
    
    In part of writing the new code that fixes this issue, I modernized some of the Region class, removing some internal methods that don't need to be there anymore. I essentially moved `#readLocation()` directly into `readLocations()`, with the new edits though. Now it's both smaller, more compact, and it actually seems to be reading correctly, so far! Yay. Been hanging on this issue for a few months now, and it has been holding me back from wanting to work on the project. I'm still essentially unfamiliar with how to use bitwise operators, and I think that's what messed me up. I was making the calculations wrong.

--- src/region.ts
+++ src/region.ts
@@ -8,35 +8,26 @@ export interface Location {
 export class Region extends Array<Chunk> {
   static async read(data: Uint8Array) {
     const locations = this.readLocations(data);
-    const chunks = await Promise.all(locations.map(location => this.#readChunk(data,location)));
+    const chunks = await Promise.all(locations.map(location => this.readChunk(data,location)));
     return new Region(...chunks);
   }
 
   static readLocations(data: Uint8Array) {
     const locations = data.subarray(0,4096);
     const view = new DataView(locations.buffer,locations.byteOffset,locations.byteLength);
-    const offset = view.getUint8(0);
-
     const result: Location[] = [];
 
-    for (let i = offset; i < locations.byteLength; i += 4){
-      const data = locations.subarray(i,i + 4);
-      const location = this.#readLocation(data);
-      result.push(location);
+    for (let i = 0; i < locations.byteLength; i += 4){
+      const offset = (view.getUint32(i) >> 8) * 4096;
+      const length = view.getUint8(i + 3) * 4096;
+      result.push({ offset, length });
       // break; // For testing
     }
 
     return result;
   }
 
-  static #readLocation(data: Uint8Array): Location {
-    const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
-    const offset = view.getUint32(0) & 0xFFFFFF * 4096;
-    const length = view.getUint8(3) * 4096;
-    return { offset, length };
-  }
-
-  static async #readChunk(data: Uint8Array, { offset, length }: Location) {
+  static async readChunk(data: Uint8Array, { offset, length }: Location) {
     const chunk = data.subarray(offset,offset + length);
     return Chunk.read(chunk);
   }
--- test/index.js
+++ test/index.js
@@ -7,12 +7,13 @@ const data = await fs.readFile(new URL("./world/r.0.0.mcr",import.meta.url))
   .then(buffer => new Uint8Array(buffer));
 // console.log(data);
 
-// const region = Region.readLocations(data);
+const region = await Region.read(data);
 
-// for (const location of region){
-//   console.log(location);
-// }
+for (const { header } of region){
+  console.log(header);
+}
 
+/*
 const content = data.subarray(0,4096);
 const view = new DataView(content.buffer,content.byteOffset,content.byteLength);
 
@@ -21,11 +22,12 @@ let result = "";
 for (let i = 0; i < content.byteLength; i += 4){
   // result += `${data.subarray(i,i + 4).join(" ")}\n`;
   // result += `${new Int32Array(data.buffer.slice(i,i + 4))}\n`;
-  const location = view.getUint32(i);
+  // const location = view.getUint32(i);
   // Thanks UtterEvergreen!!!
-  const offset = (location >> 8) * 4096;
-  const length = (location & 255) * 4096;
+  const offset = (view.getUint32(i) >> 8) * 4096;
+  const length = view.getUint8(i + 3) * 4096;
   result += `${offset} ${length}\n`;
 }
 
 console.log(result);
+*/
