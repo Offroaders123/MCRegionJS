187258a03957575a2ad22dadfc468973e3170e8c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-01 02:07:17 -0800

    Stable-Code: Part Three
    This one just adds the new names I gave to the properties and such, to help better describe to myself what each of them does. Now they also are similar to the kinds of names I have been using throughout other parts of my code, like over in NBTify.

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,27 +1,34 @@
 import { inflateRaw, rld } from "./compression.js";
 
+export interface Header {
+  isRLE: boolean;
+  compressedLength: number;
+  decompressedLength: number;
+  RLECompressedLength: number;
+}
+
 export class Chunk {
   static async read(data: Uint8Array) {
-    const { rleFlag, sizeOfChunkData, decompressedSize, decSize } = this.readHeader(data);
+    const { isRLE, compressedLength, decompressedLength, RLECompressedLength } = this.readHeader(data);
 
-    const abyte = data.subarray(12);
-    const decompressedData = new Uint8Array(decSize);
+    const compressedData = data.subarray(12);
+    const RLECompressedData = new Uint8Array(RLECompressedLength);
 
-    decompressedData.set(await inflateRaw(abyte));
+    RLECompressedData.set(await inflateRaw(compressedData));
 
-    const rle = rld(decompressedData,decompressedSize);
+    const decompressedData = rld(RLECompressedData,decompressedLength);
 
-    return Object.assign(new Chunk(),{ rle });
+    return Object.assign(new Chunk(),{ decompressedData });
   }
 
   static readHeader(data: Uint8Array) {
     const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
 
-    const rleFlag = Boolean(view.getUint8(0) >> 7);
-    const sizeOfChunkData = view.getUint32(0) & 0x3FFFFFFF;
-    const decompressedSize = view.getUint32(4);
-    const decSize = view.getUint32(8);
+    const isRLE = Boolean(view.getUint8(0) >> 7);
+    const compressedLength = view.getUint32(0) & 0x3FFFFFFF;
+    const decompressedLength = view.getUint32(4);
+    const RLECompressedLength = view.getUint32(8);
 
-    return { rleFlag, sizeOfChunkData, decompressedSize, decSize };
+    return { isRLE, compressedLength, decompressedLength, RLECompressedLength } as Header;
   }
 }
