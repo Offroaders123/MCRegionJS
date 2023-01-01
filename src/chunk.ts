078d49c6d64c03183df2232c9118f34332e7a01f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-01 15:15:44 -0800

    ChunkData Header
    This is the commit that adds the parsing for the inner chunk headers. This has things like the format version for the chunk, it's x and y position, it's last updated time, and it's inhabited time, which I'm not quite sure what that one is.

--- src/chunk.ts
+++ src/chunk.ts
@@ -7,11 +7,20 @@ export interface CompressionHeader {
   decompressedLength: number;
 }
 
+export interface Header {
+  format: number;
+  x: number;
+  y: number;
+  lastUpdate: bigint;
+  inhabited: bigint;
+}
+
 export class Chunk {
   static async read(data: Uint8Array) {
     const decompressedData = await this.decompress(data);
+    const header = this.readHeader(decompressedData);
 
-    return { data: decompressedData };
+    return { ...header, data: decompressedData.subarray(26) };
   }
 
   static readCompressionHeader(data: Uint8Array) {
@@ -33,6 +42,18 @@ export class Chunk {
     const decompressedData = rleDecode(RLECompressedData,decompressedLength);
 
     return decompressedData;
+  }
+
+  static readHeader(data: Uint8Array) {
+    const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
+
+    const format = view.getUint16(0);
+    const x = view.getUint32(2);
+    const y = view.getUint32(6);
+    const lastUpdate = view.getBigUint64(10);
+    const inhabited = view.getBigUint64(18);
+
+    return { format, x, y, lastUpdate, inhabited } as Header;
   }
 
   constructor(public data: Uint8Array) {}
