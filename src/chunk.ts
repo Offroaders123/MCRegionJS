e6109df960e5e50721aec926b88728318d89aaeb
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-01 01:46:28 -0800

    Stable-Code: Part Two
    This update splits the chunk reading method up into two, one for the header portion, and one for the chunk data itself.
    
    Forgot to say it in the last commit, Happy New Year!!! :D
    
    Just finished listening to Mike Keneally Dog,
    now I'm on to...
     ______
    |P/T   | \w/
    |C/C   |
    |______|

--- src/chunk.ts
+++ src/chunk.ts
@@ -2,12 +2,7 @@ import { inflateRaw, rld } from "./compression.js";
 
 export class Chunk {
   static async read(data: Uint8Array) {
-    const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
-
-    const rleFlag = Boolean(view.getUint8(0) >> 7);
-    const sizeOfChunkData = view.getUint32(0) & 0x3FFFFFFF;
-    const decompressedSize = view.getUint32(4);
-    const decSize = view.getUint32(8);
+    const { rleFlag, sizeOfChunkData, decompressedSize, decSize } = this.readHeader(data);
 
     const abyte = data.subarray(12);
     const decompressedData = new Uint8Array(decSize);
@@ -16,6 +11,17 @@ export class Chunk {
 
     const rle = rld(decompressedData,decompressedSize);
 
-    return Object.assign(new Chunk(),{ rleFlag, sizeOfChunkData, decompressedSize, decSize, rle });
+    return Object.assign(new Chunk(),{ rle });
+  }
+
+  static readHeader(data: Uint8Array) {
+    const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
+
+    const rleFlag = Boolean(view.getUint8(0) >> 7);
+    const sizeOfChunkData = view.getUint32(0) & 0x3FFFFFFF;
+    const decompressedSize = view.getUint32(4);
+    const decSize = view.getUint32(8);
+
+    return { rleFlag, sizeOfChunkData, decompressedSize, decSize };
   }
 }
