e582e00fd6e8a9391ad00ccf31ae83f7c168918f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-01 16:41:16 -0800

    NBT Search
    Now that NBTify is extra stable, with things like it's reading checksum and format auto-detection, I set up a little script to try and find any NBT inside of the chunk. No luck! I'm curious what the text snippets at the end of the chunk are. You can see them if you stringify the bytes from the chunk. That's a commented out part of this test script too (the `Buffer.toString()` part), if you wanted to see what that looks like.
    
    This code was originally from way back when I wrote that `anvil.mjs` file, back in October it looks like. I just modified it a bit and put it back into the test script for the current version of the library.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 import * as fs from "node:fs/promises";
+import * as NBT from "nbtify";
 import { Region } from "../dist/index.js";
 
 const data = await fs.readFile(new URL("./world/r.0.0.mcr",import.meta.url))
@@ -8,8 +9,38 @@ const data = await fs.readFile(new URL("./world/r.0.0.mcr",import.meta.url))
 // console.log(data);
 
 // const region = await Region.read(data);
-const region = [await Region.readChunk(data,Region.readLocations(data)[300])];
+// const region = [await Region.readChunk(data,Region.readLocations(data)[300])];
 
-for (const chunk of region){
-  console.log(chunk);
+// for (const chunk of region){
+//   console.log(chunk);
+// }
+
+const region = await Region.readChunk(data,Region.readLocations(data)[300]);
+// console.log(Buffer.from(region?.data).toString());
+
+if (region !== null){
+  const { data } = region;
+  console.log(data.byteLength);
+
+  const nbt = await findNBT(data);
+  console.log(nbt);
+}
+
+/**
+ * Tries to find an NBT structure within a set of data.
+ * 
+ * @param { ArrayBufferLike | Uint8Array } data
+ * @param { number } offset
+ * @returns { Promise<NBT.NBTData> }
+*/
+async function findNBT(data,offset = 0){
+  try {
+    console.log(offset);
+    if (offset > data.byteLength) throw new Error("Reached the end of the buffer! No NBT data was found");
+    // return await NBT.read(data);
+    return await NBT.read(data,{ endian: "big", compression: null, isNamed: true, isBedrockLevel: false });
+  } catch (error){
+    if (offset > data.byteLength) throw error;
+    return findNBT(data,offset += 1);
+  }
 }
