92d9eacdddbf8c51148451257da8875a5e96e128
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-05 22:09:34 -0700

    Single-Parse Function
    Brought the Chunk parsing steps into a single function, since I want to more easily figure out how everything works procedurally, before trying to abstract certain parts out. I think this will help with making a more singular Chunk parsing algorithm, because some parts of the implementation can more simply borrow/share parts from the others. For example, all of the platforms need to handle Chunks, but some of them change what kind of 'Chunk' type they return, as well as what compression and endian types are used. Somehow I was going to mention in that last sentence how Region files are only present on some platforms too, notably missing from Bedrock-based platforms.
    
    I had to listen to the start of 'The Car Song' multiple times over, after the first listen. Absolutely outstanding.

--- src/chunk.ts
+++ src/chunk.ts
@@ -6,69 +6,26 @@ import type { Region, Entry } from "./region.js";
 export interface Chunk {}
 
 export async function readChunks(region: Region): Promise<(Chunk | null)[]> {
-  return Promise.all(region.map(async entry => {
-    entry = await decompressChunk(entry);
-    if (entry === null) return null;
-    const header = readHeader(entry);
-    // if (header.Format !== 12) return null;
-    console.log(header);
-
-    for (let i = entry.byteLength; i > 0; i--){
-      try {
-        const nbt = await read(entry.subarray(i),{ name: "", endian: "big", compression: null, bedrockLevel: null });
-        // console.log(nbt.data,"\n");
-        return nbt;
-      } catch (error){
-        continue;
-      }
-    }
-
-    return null;
-  }));
+  return Promise.all(region.map(readEntry));
 }
 
 const COMPRESSION_HEADER_LENGTH = 12;
 
-interface CompressionHeader {
-  rle: boolean;
-  compressedLength: number;
-  rleCompressedLength: number;
-  decompressedLength: number;
-}
+export async function readEntry(entry: Entry): Promise<Chunk | null> {
+  if (entry === null || entry.byteLength < COMPRESSION_HEADER_LENGTH) return null;
 
-function readCompressionHeader(entry: Uint8Array): CompressionHeader {
-  const view = new DataView(entry.buffer,entry.byteOffset,entry.byteLength);
+  let view = new DataView(entry.buffer,entry.byteOffset,entry.byteLength);
 
   const rle = Boolean(view.getUint8(0) >> 7);
   const compressedLength = view.getUint32(0) & 0x3FFFFFFF;
   const decompressedLength = view.getUint32(4);
   const rleCompressedLength = view.getUint32(8);
 
-  return { rle, compressedLength, rleCompressedLength, decompressedLength };
-}
-
-async function decompressChunk(entry: Entry): Promise<Entry> {
-  if (entry === null || entry.byteLength < COMPRESSION_HEADER_LENGTH) return null;
-
-  const { decompressedLength } = readCompressionHeader(entry);
-
   const compressedEntry = entry.subarray(COMPRESSION_HEADER_LENGTH);
   const rleCompressedEntry = await decompress(compressedEntry,"deflate-raw");
   const decompressedEntry = runLengthDecode(rleCompressedEntry,decompressedLength);
 
-  return decompressedEntry;
-}
-
-interface Header {
-  Format: number;
-  X: number;
-  Y: number;
-  LastUpdate: bigint;
-  Inhabited: bigint;
-}
-
-function readHeader(data: Uint8Array): Header {
-  const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
+  view = new DataView(decompressedEntry.buffer,decompressedEntry.byteOffset,decompressedEntry.byteLength);
 
   const Format = view.getUint16(0);
   const X = view.getUint32(2);
@@ -76,5 +33,17 @@ function readHeader(data: Uint8Array): Header {
   const LastUpdate = view.getBigUint64(10);
   const Inhabited = view.getBigUint64(18);
 
-  return { Format, X, Y, LastUpdate, Inhabited };
+  console.log({ Format, X, Y, LastUpdate, Inhabited });
+
+  for (let i = decompressedEntry.byteLength; i > 0; i--){
+    try {
+      const nbt = await read<Chunk>(decompressedEntry.subarray(i),{ name: "", endian: "big", compression: null, bedrockLevel: null });
+      // console.log(nbt.data,"\n");
+      return nbt;
+    } catch (error){
+      continue;
+    }
+  }
+
+  return null;
 }
