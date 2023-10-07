3037c1de11d6394f1fce0d6eae627ebefa760c3b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-07 02:02:19 -0700

    Section Parsing
    Extra big props to Evergreen, I still don't know how this part works. I just need to get it to work, then I can reverse engineer how it works in order to refactor it and make it nicer.
    
    And yes, I'm going to move these into distinct functions down the line. I'm embedding these all into a single function at the moment because it helps me the most to see how it works at the procedural level.
    
    The functions at the bottom of the file are essentially just placeholders for the actual Block/Submerged parsing code, I added those so the loops can run properly, allowing the buffer offsets to be read accordingly. That's my initial goal, I want to ensure I'm reading this part correctly by getting to the data following these sections.
    
    Once those are ensured, then I will come back and properly write these section readings to the Blocks and Submerged arrays accordingly.
    
    The part from `sectionJumpTable` downwards was in a class method called `parseBlocks`, so that's why those weird break statements are in there. It previously just returned from inside that function, as this one performed side-effects on the class' properties.
    
    I also kept around the comments from the original code.
    
    This was originally C++ code, I had to refactor it into TS code. That was a very interesting thing to figure out actually, now I'm understanding things from C(++)'s syntax a bit more now! Like how to allocate new buffers and things like that. You essentially use the data type as the constructor name, then just construct it with plain parenthesis, who knew? That's actually really clever!
    
    "I don't know what you mean.."
    Ok, here's an example:
    ```cpp
    std::vector<uint16_t> sectionJumpTable(16);
    ```
    ```ts
    const sectionJumpTable = new Uint16Array(16);
    ```
    Here's what a hybrid approach could look like with TS syntax:
    ```ts
    const sectionJumpTable: Uint16Array = (16);
    ```
    
    I might be doing these here wrong, though.. I thought these were arrays too, actually?
    ```cpp
    uint16_t grid[128];
    uint16_t submergedData[128];
    ```
    ```ts
    const grid = new Uint16Array(128);
    const submergedData = new Uint16Array(128);
    ```
    I have a feeling these are just plain `short` values though, rather than a buffer/array. I'll come back and figure this one out with the full section parsing implementation later on.

--- src/chunk.ts
+++ src/chunk.ts
@@ -72,7 +72,7 @@ export async function readEntry(entry: Entry): Promise<Chunk | null> {
   const maxSectionAddress = view.getUint16(0) << 8;
   console.log(maxSectionAddress);
 
-  const sectionJumpTable = new Uint16Array(16);
+  const sectionJumpTable = new Uint16Array(16);//read 16 shorts so 32 bytes
   for (let i = 0; i < sectionJumpTable.length; i++){
     const address = view.getUint16(i + 2);
     sectionJumpTable[i] = address;
@@ -84,5 +84,131 @@ export async function readEntry(entry: Entry): Promise<Chunk | null> {
 
   view = new DataView(view.buffer,view.byteOffset + 50,view.byteLength - 50);
 
+  if (maxSectionAddress !== 0){
+    parseBlocks: for (let section = 0; section < 16; section++){
+      const address = sectionJumpTable[section]!;
+      view = new DataView(view.buffer,view.byteOffset + 76 + address,view.byteLength - 76 - address);
+      if (address === maxSectionAddress){
+        break;
+      }
+      if (!sizeOfSubChunks[section]){
+        continue;
+      }
+      const sectionHeader = new Uint8Array(view.buffer,view.byteOffset + 0x80,0x80);
+      view = new DataView(view.buffer,view.byteOffset + 0x80,view.byteLength - 0x80);
+      for (let gx = 0; gx < 4; gx++){
+        for (let gz = 0; gz < 4; gz++){
+          for (let gy = 0; gy < 4; gy++){
+            const gridIndex = gx * 16 + gz * 4 + gy;
+
+            const v1 = sectionHeader[gridIndex * 2]!;
+            const v2 = sectionHeader[gridIndex * 2 + 1]!;
+            const t1 = v1 >> 4;
+            const t2 = 0xf & v1;
+            const t3 = v2 >> 4;
+            const t4 = 0xf & v2;
+
+            const offset = (t4 << 8 | t1 << 4 | t2) * 4;
+            const format = t3;
+
+            const grid = new Uint16Array(128);
+            const submergedData = new Uint16Array(128);
+            const gridPosition = 0xcc + address + offset;//0x4c for start and 0x80 for header
+            const offsetInBlockWrite = section * 0x20 + gy * 8 + gz * 0x800 + gx * 0x8000;
+            if (format === 0){
+              singleBlock(v1, v2, grid);
+            } else if (format === 0xf){//read 128 bytes for normal blocks plus 128 bytes for submerged blocks
+              /*if (gridPosition + 128 >= view.byteLength){
+                break parseBlocks;
+              }
+              ParseFormatF(view.byteOffset + gridPosition, grid);*/
+              if (gridPosition + 256 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+              maxBlocks(view.byteOffset + gridPosition, grid);
+              maxBlocks(view.byteOffset + gridPosition + 128, submergedData);
+              putBlocks(Submerged, submergedData, offsetInBlockWrite);
+            } else if (format === 0xe){//read 128 bytes for normal blocks 
+              if (gridPosition + 128 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+              maxBlocks(view.byteOffset + gridPosition, grid);
+            } else if (format === 0x2){ // 1 bit
+              if (gridPosition + 12 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parse<1>(view.byteOffset + gridPosition, grid)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+            } else if (format === 0x3){ // 1 bit + submerged
+              if (gridPosition + 20 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parseWithLayers<1>(view.byteOffset + gridPosition, grid, submergedData)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+              putBlocks(Submerged, submergedData, offsetInBlockWrite);
+            } else if (format === 0x4){ // 2 bit
+              if (gridPosition + 24 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parse<2>(view.byteOffset + gridPosition, grid)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+            } else if (format === 0x5){ // 2 bit + submerged
+              if (gridPosition + 40 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parseWithLayers<2>(view.byteOffset + gridPosition, grid, submergedData)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+              putBlocks(Submerged, submergedData, offsetInBlockWrite);
+            } else if (format === 0x6){ // 3 bit
+              if (gridPosition + 40 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parse<3>(view.byteOffset + gridPosition, grid)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+            } else if (format === 0x7){ // 3 bit + submerged
+              if (gridPosition + 64 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parseWithLayers<3>(view.byteOffset + gridPosition, grid, submergedData)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+              putBlocks(Submerged, submergedData, offsetInBlockWrite);
+            } else if (format === 0x8){ // 4 bit
+              if (gridPosition + 64 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parse<4>(view.byteOffset + gridPosition, grid)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+            } else if (format === 0x9){ // 4bit + submerged
+              if (gridPosition + 96 >= view.byteLength) /*[[unlikely]]*/ {
+                break parseBlocks;
+              }
+                if (!parseWithLayers<4>(view.byteOffset + gridPosition, grid, submergedData)) /*[[unlikely]]*/ {
+                  break parseBlocks;
+                }
+              putBlocks(Submerged, submergedData, offsetInBlockWrite);
+            } else {
+              break parseBlocks;//this should never occur
+            }
+
+            putBlocks(Blocks, grid, offsetInBlockWrite);
+          }
+        }
+      }
+    }
+  }
+
   return new NBTData<ChunkData>({ Format, X, Y, LastUpdate, Inhabited, Blocks, Submerged });
 }
+
+function parse<_T>(_offset: number, _grid: Uint16Array): boolean { return undefined as unknown as boolean; }
+function parseWithLayers<_T>(_offset: number, _grid: Uint16Array, _submergedGrid: Uint16Array): boolean { return undefined as unknown as boolean; }
+function singleBlock(_block1: number, _block2: number, _data: Uint16Array){}
+function putBlocks(_blocks: ShortTag[], _data: Uint16Array, _offset: number){}
+function maxBlocks(_block: number, _data: Uint16Array){}
