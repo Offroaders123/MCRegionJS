2c222e60876157104ec020ef946b836870f4dcd1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-05-13 12:35:23 -0700

    Java/LCE Region Handling
    Working on bridging the gap between these two implementations, and adding parts to distinguish better where each's own format starts, and where they are the same.
    
    https://github.com/Offroaders123/MCA-Buffer/commit/872e24265006e84e541ae2bb100ecac175c749a6

--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,21 @@
 import { readFile } from "node:fs/promises";
-import { readRegion, readEntry } from "../src/index.js";
+import { chunkCoordinateFromIndex, indexFromChunkCoordinate, readRegion } from "../src/index.js";
 
 const REGION = new URL("./world/r.0.0.mcr",import.meta.url);
 
 const data = await readFile(REGION);
 
 const region = readRegion(data);
-// console.log(region[64]);
-// console.log(region.length);
-
-const chunk = await readEntry(region[64]!);
-console.log(chunk);
-// console.log(chunks.length);
+console.log(
+  // new Set(
+  region
+    .slice(1000)
+    // .map(chunk => {
+    //   const { index } = chunk;
+    //   const coords = chunkCoordinateFromIndex(index);
+    //   const diffIndex = indexFromChunkCoordinate(coords);
+    //   // return index === diffIndex;
+    //   return [index, coords, diffIndex];
+    // })
+  // )
+);
